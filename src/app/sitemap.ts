f1304c4b2e44e9547c2674137d908142e40c27ce
IvailoAtsv <105544716+IvailoAtsv@users.noreply.github.com>
2025-07-10 16:37:25 +0300

    fix: robots and sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -11,23 +11,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'monthly',
       priority: 1,
     },
-    {
-      url: `${baseUrl}/contact`,
-      lastModified: currentDate,
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/projects`,
-      lastModified: currentDate,
-      changeFrequency: 'monthly',
-      priority: 0.9,
-    },
-    {
-      url: `${baseUrl}/experience`,
-      lastModified: currentDate,
-      changeFrequency: 'monthly',
-      priority: 0.9,
-    },
   ];
 }
